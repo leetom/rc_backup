4e921ac9440224979cf9f2cba54bc4af7cd183b6
leetom <leetom1991@163.com>
2013-12-13 20:31:12 +0800

    add new file

---
+++ dumpremotesql.sh
@@ -0,0 +1,1 @@
+mysqldump -hscratch.codepku.com -udrupal -p drupal7 --ignore-table=drupal7.cache --ignore-table=drupal7.cache_form > ~/drupal7.sql
---
+++ getremotesql.sh
@@ -0,0 +1,1 @@
+scp -P 2222 leetom@scratch.codepku.com:~/drupal7.sql ~/drupal7.sql
